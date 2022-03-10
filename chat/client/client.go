c4e9fc70c8f9fe91ac1375b6cd580871533ce377
huangqiuhua <huangqiuhua2014@xiaochuankeji.cn>
2022-03-10 22:59:05 +0800

    dev chat example

--- chat/client/client.go
+++ chat/client/client.go
@@ -1,12 +1,23 @@
 package main
 
 import (
+	"bati-chat/proto"
 	"context"
 	"flag"
+	"fmt"
+	"log"
+	"math/rand"
+	"os"
 	"time"
 
 	"github.com/batigo/baticli-go"
 )
+
+var randd *rand.Rand
+
+func init() {
+	randd = rand.New(rand.NewSource(int64(os.Getpid())))
+}
 
 func main() {
 	var did string
@@ -14,10 +25,15 @@ func main() {
 	var dt int
 	var compressor string
 	var url string
+	var room string
+	var name string
+
 	flag.StringVar(&url, "url", "", "remote ws url")
 	flag.StringVar(&did, "did", "", "device id")
 	flag.StringVar(&uid, "uid", "", "user id")
 	flag.StringVar(&compressor, "compressor", "null", "compressor type: null | deflate ")
+	flag.StringVar(&room, "room", "room1", "join room id")
+	flag.StringVar(&name, "name", "name1", "user name")
 	flag.IntVar(&dt, "dt", 1, "device type ")
 	flag.Parse()
 
@@ -31,9 +47,85 @@ func main() {
 		Compressor: baticli.CompressorType(compressor),
 		BinaryMsg:  false,
 	}
-	_, err := baticli.NewConn(context.Background(), conf)
+	cli, sendmsgFunc, err := baticli.NewConn(context.Background(), conf)
 	if err != nil {
 		panic(err)
 	}
 
+	stopch := make(chan interface{})
+
+	cli.SetConnCloseHanler(func() {
+		close(stopch)
+		cli.Close()
+	})
+
+	cli.SetRecvMsgHandler(func(msg baticli.ClientMsgRecv) {
+		switch msg.Type {
+		case baticli.ClientMsgTypeAck:
+			log.Printf("=== recv ack msg, id: %s\n", msg.Id)
+		case baticli.ClientMsgTypeBiz:
+			chatMsg := &proto.ChatMsgRecv{}
+			chatMsg.Decode(msg.Data)
+			switch chatMsg.Type {
+			case proto.MsgTypeJoinRoom:
+				var join proto.JoinRoomData
+				join.Decode(chatMsg.Data)
+				log.Printf("=== %s\n", join.String())
+			case proto.MsgTypeQuitRoom:
+				var quit proto.QuitRoomData
+				quit.Decode(chatMsg.Data)
+				log.Printf("=== %s\n", quit.String())
+			case proto.MsgTypeChat:
+				var chat proto.ChatData
+				chat.Decode(chatMsg.Data)
+				log.Printf("=== %s\n", chat.String())
+			}
+		default:
+			log.Printf("recv unknown msg, type: %d\n", msg.Type)
+		}
+	})
+
+	err = cli.Init()
+	if err != nil {
+		panic(err)
+	}
+
+	joinmsg := proto.ChatMsgSend{
+		Type: proto.MsgTypeJoinRoom,
+		Data: proto.JoinRoomData{Id: room, Name: name},
+	}
+	sendmsgFunc(baticli.ClientMsgSend{
+		Id:        baticli.Genmsgid(),
+		Type:      baticli.ClientMsgTypeBiz,
+		Ack:       1,
+		ServiceId: "chat",
+		Data:      joinmsg,
+	})
+
+	for i := 0; i < 30+randd.Intn(20); i++ {
+		chatmsg := proto.ChatMsgSend{
+			Type: proto.MsgTypeChat,
+			Data: proto.ChatData{Name: name, Msg: fmt.Sprintf("msg-%d", i)},
+		}
+		sendmsgFunc(baticli.ClientMsgSend{
+			Id:        baticli.Genmsgid(),
+			Type:      baticli.ClientMsgTypeBiz,
+			Ack:       1,
+			ServiceId: "chat",
+			Data:      chatmsg,
+		})
+	}
+
+	quitmsg := proto.ChatMsgSend{
+		Type: proto.MsgTypeQuitRoom,
+		Data: proto.QuitRoomData{Id: room, Name: name},
+	}
+	sendmsgFunc(baticli.ClientMsgSend{
+		Id:        baticli.Genmsgid(),
+		Type:      baticli.ClientMsgTypeBiz,
+		Ack:       1,
+		ServiceId: "chat",
+		Data:      quitmsg,
+	})
+	cli.Close()
 }
--- chat/proto/proto.go
+++ chat/proto/proto.go
@@ -1,5 +1,10 @@
 package proto
 
+import (
+	"encoding/json"
+	"fmt"
+)
+
 const (
 	MsgTypeJoinRoom = 1
 	MsgTypeQuitRoom = 2
@@ -7,13 +12,54 @@ const (
 )
 
 type JoinRoomData struct {
-	Id string `json:"id"`
+	Id   string `json:"id"`
+	Name string `json:"name"`
+}
+
+func (d *JoinRoomData) String() string {
+	return fmt.Sprintf("[join] from: %s", d.Name)
+}
+
+func (d *JoinRoomData) Decode(bs json.RawMessage) {
+	json.Unmarshal(bs, d)
 }
 
 type QuitRoomData struct {
-	Id string `json:"id"`
+	Id   string `json:"id"`
+	Name string `json:"name"`
+}
+
+func (d *QuitRoomData) String() string {
+	return fmt.Sprintf("[quit] from: %s", d.Name)
+}
+
+func (d *QuitRoomData) Decode(bs json.RawMessage) {
+	json.Unmarshal(bs, d)
 }
 
 type ChatData struct {
-	Msg string `json:"msg"`
+	Name string `json:"name"`
+	Msg  string `json:"msg"`
+}
+
+func (d *ChatData) String() string {
+	return fmt.Sprintf("[chat] from: %s, msg: %s", d.Name, d.Msg)
+}
+
+func (d *ChatData) Decode(bs json.RawMessage) {
+	json.Unmarshal(bs, d)
+}
+
+type ChatMsgRecv struct {
+	Type int             `json:"type"`
+	Data json.RawMessage `json:"data"`
+}
+
+func (d *ChatMsgRecv) Decode(bs json.RawMessage) {
+	json.Unmarshal(bs, d)
+}
+
+type ChatMsgSend struct {
+	Type int         `json:"type"`
+	Data interface{} `json:"data"`
 }
